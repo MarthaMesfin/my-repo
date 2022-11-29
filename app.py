cff2612165a12f144b6a6240b8a7b20838ca0e88
MyProject1412 <noreasonmesfin26@gmail.com>
2022-11-29 15:43:26 +0300

    function taht excute sth..

---
+++ app.py
@@ -0,0 +1,31 @@
+# print('Hello wprld')
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___| ")
+
+# char_name = 'John'
+#his_age = '35'
+# char_name = 'Tom'
+# his_age = '25'
+# salary = 275000.00
+# is_male = True 
+# print('There was a man named ' + char_name + ', ')
+# print('he is ' + his_age + ' years old'+ '. ' )
+# char_name = 'Mike'
+# print('He realy liked the name ' + char_name + ', ')
+# print('but he did not like being ' + his_age + '. ' )
+
+# print('Hello\nMarina')
+
+# phrase = 'Giraffe Academy'
+# appp = 'hello'
+# ccc = 'hey'
+# print(phrase.lower())
+# print(phrase.capitalize())
+# print(appp.upper())
+# print(ccc.upper().isupper())
+# print(len(phrase))
+
+def temDefine():
+    print('Abcd')
